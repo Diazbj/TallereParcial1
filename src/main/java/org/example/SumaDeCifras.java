55984e46ec3b2258ee51ac188f840b14b08a6f13
Jordy Diaz <diaz.jordyb@gmail.com>
2024-02-18 01:00:41 -0500

    ejercicio 3 finalizado

---
+++ src/main/java/org/example/SumaDeCifras.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class SumaDeCifras {
+
+    public static void main(String[] args){
+
+        int numero= 45879632;
+
+        int suma=SumaCifras(numero);
+
+        System.out.println(suma);
+
+    }
+
+    public static int SumaCifras(int numero){
+
+        if(numero==0){
+            return 0;
+        }else{
+            return SumaCifras(numero/10)+numero%10;
+        }
+
+
+    }
+
+}
