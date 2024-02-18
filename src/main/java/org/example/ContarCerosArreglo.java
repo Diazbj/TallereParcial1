9121cb5d9592b35941f7bc3b86e579b534f8e5ac
Jordy Diaz <diaz.jordyb@gmail.com>
2024-02-18 00:22:48 -0500

    2 resueltos

--- src/main/java/org/example/ContarCerosArreglo.java
+++ src/main/java/org/example/ContarCerosArreglo.java
@@ -6,8 +6,26 @@ public class ContarCerosArreglo {
 
     public static void main(String[] args){
 
-        int[] arreglo = {1, 2, 3, 4, 5};
+        int indice=0;
+        int[] arreglo = {1, 2, 0, 4, 0};
 
+        int ceros=ContarCeros(arreglo,indice);
+
+        System.out.println(ceros);
 
     }
+
+    public static int ContarCeros(int[] arreglo,int indice){
+
+        if(indice==arreglo.length){
+            return 0;
+        }else{
+            if(arreglo[indice]==0){
+                return ContarCeros(arreglo,indice+1)+1;
+            }else{
+                return ContarCeros(arreglo,indice+1);
+            }
+        }
+    }
+
 }
