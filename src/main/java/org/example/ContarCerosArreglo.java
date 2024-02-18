7a6e5bfda923f2efdff6ab4c89c977a094c17dc0
Jordy Diaz <diaz.jordyb@gmail.com>
2024-02-17 23:59:51 -0500

    contar ceros

--- src/main/java/org/example/ContarCerosArreglo.java
+++ src/main/java/org/example/ContarCerosArreglo.java
@@ -1,4 +1,13 @@
 package org.example;
 
+import java.lang.reflect.Array;
+
 public class ContarCerosArreglo {
+
+    public static void main(String[] args){
+
+        int[] arreglo = {1, 2, 3, 4, 5};
+
+
+    }
 }
