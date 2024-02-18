6e23af572bae7c862aa7de6bf5afa11e6aca5440
Jordy Diaz <diaz.jordyb@gmail.com>
2024-02-17 19:45:58 -0500

    cifras

--- src/main/java/org/example/CantidadCifras.java
+++ src/main/java/org/example/CantidadCifras.java
@@ -1,9 +1,23 @@
 package org.example;
 
+//Hallar la cantidad de cifras de un numero entero.
+
 public class CantidadCifras {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        int n=1234567891;
+        int cifras=CalcularCantidadCifras(n);
+        System.out.println(cifras);
 
     }
-}
+
+    public static int CalcularCantidadCifras(int n){
+
+        if(n/10<10){
+            return 2;
+        }else {
+
+            return CalcularCantidadCifras(n/10)+1;
+            }
+        }
+    }
