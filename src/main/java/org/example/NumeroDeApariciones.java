bb47d457352c439c057a632fdcadc29751a0dbb3
Jordy Diaz <142914485+Diazbj@users.noreply.github.com>
2024-02-20 02:03:34 -0500

    final ejercicio 5

--- src/main/java/org/example/NumeroDeApariciones.java
+++ src/main/java/org/example/NumeroDeApariciones.java
@@ -1,4 +1,37 @@
 package org.example;
 
 public class NumeroDeApariciones {
+
+     public static void main(String[] args){
+         int[] arreglo={1,1,2,2,2,0,12};
+
+         int elemento=-10;
+         int indice=0;
+
+         int apariciones=BuscarElemento(arreglo,elemento,indice);
+
+         System.out.println(apariciones);
+     }
+
+    public static int BuscarElemento(int[] arreglo,int elemento,int indice){
+
+         if(indice==arreglo.length){
+
+             return 0;
+
+         }else{
+             if(elemento==arreglo[indice]){
+
+                 return BuscarElemento(arreglo,elemento,indice+1)+1;
+
+             }else {
+                 return BuscarElemento(arreglo,elemento,indice+1);
+
+             }
+         }
+
+    }
+
 }
+
+
