d735f7f47a8308ab4864ae836d384958b0f42caa
Jordy Diaz <diaz.jordyb@gmail.com>
2024-03-07 00:19:44 -0500

    avances

--- src/main/java/org/example/DigonalMatriz13.java
+++ src/main/java/org/example/DigonalMatriz13.java
@@ -18,25 +18,13 @@ public class DigonalMatriz13 {
 
     private static int sumarDiagonal(int[][] matriz, int i, int j) {
 
-        if(i == matriz.length || j == matriz[i].length){
-            System.out.println("Fin de la matriz");
-        }else{
-            if(i==j){
-                return sumarDiagonal(matriz, i, j)+matriz[i][j];
-                j++;
-            }
-            System.out.print(" "+matriz[i][j]);
-            if(j == matriz[i].length-1){
-                i++;
-                j = 0;
-                System.out.println("");
-            }else{
-                j++;
-                return sumarDiagonal(matriz, i, j)+matriz[i][j];
-            }
-            sumarDiagonal(matriz, i, j);
+        if (i == matriz.length || j == matriz[i].length) {
+            return 0;
+        } else if (i == j) {
+            return sumarDiagonal(matriz, i, j+1) + matriz[i][j];
+        } else {
+            return sumarDiagonal(matriz, i + 1, j);
         }
-        return 0;
     }
 }
 
